0bb6952ac994752e5fa0193fcef5d6b81aced6d2
jacobolenick <jacobolenick@Jacobs-MacBook-Pro.local>
2021-03-19 17:34:59 -0700

    updated js files

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,57 +1,72 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
-const Mission = () => {
+const mapStateToProps = (state) => {
+  return {
+    partners: state.partners,
+  };
+};
+function Mission() {
   return (
     <Card title="Our Mission">
-      <Text style ={{margin: 10}}>
-        We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-    </Text>
+      <Text style={{ margin: 10 }}>
+        We present a curated database of the best campsites in the vast woods
+        and backcountry of the World Wide Web Wilderness. We increase access to
+        adventure for the public while promoting safe and respectful use of
+        resources. The expert wilderness trekkers on our staff personally verify
+        each campsite to make sure that they are up to our standards. We also
+        present a platform for campers to share reviews on campsites they have
+        visited with each other.
+      </Text>
     </Card>
-  )
+  );
 }
-
 class About extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      partners: PARTNERS
-    };
-  }
-
   static navigationOptions = {
-    title: 'About Us'
+    title: 'About Us',
   };
-
-
-
   render() {
     const renderPartner = ({ item }) => {
       return (
         <ListItem
           title={item.name}
           subtitle={item.description}
-          leftAvatar={{ source: require('./images/bootstrap-logo.png') }}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
         />
-      )
+      );
+    };
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+            <Mission />
+            <Card title="Community Partners">
+              <Text>{this.props.partners.errMess}</Text>
+            </Card>
+          </Animatable.View>
+        </ScrollView>
+      );
     }
-  return(
+    return (
       <ScrollView>
-        <Mission />
-
-      <Card title="Community Partners">
-        <FlatList
-        data={this.state.partners}
-        renderItem={renderPartner}
-        keyExtractor={item => item.id.toString()}
-        />
-      </Card>
-
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Mission />
+          <Card title="Community Partners">
+            <FlatList
+              data={this.props.partners.partners}
+              renderItem={renderPartner}
+              keyExtractor={(item) => item.id.toString()}
+            />
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
