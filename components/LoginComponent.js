de4d701d5a26abbc579940155245ddbf4a73b51a
jacobolenick <jacobolenick@Jacobs-MacBook-Pro.local>
2021-03-27 11:59:07 -0700

    added get img gallery

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -176,6 +176,21 @@ class RegisterTab extends Component {
              this.setState({imageUrl: processedImage.uri});
         
     }
+    getImageFromGallery = async () => {
+        //const cameraPermission = await Permissions.askAsync(Permissions.CAMERA);
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+    }
 
     handleRegister() {
         console.log(JSON.stringify(this.state));
@@ -204,6 +219,7 @@ class RegisterTab extends Component {
                             title='Camera'
                             onPress={this.getImageFromCamera}
                         />
+                        <Button title='Gallery' onPress={this.getImageFromGallery}/>
                     </View>
                     <Input
                         placeholder='Username'
